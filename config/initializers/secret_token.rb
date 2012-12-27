0207328d09a7ac65fbf928ebab4e06a346d5b8f2
Bryan Schoeff <schoeff@gmail.com>
2012-12-26 21:01:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BriggsAuth::Application.config.secret_token = 'c26b179d121a195e5e4938b1da37c2248cb269034d30bd348cacb09424b5023f0948ac92846dd4b41c68534c25b88bfa90a231c8d73bd16f4b6e8af6341a26ab'
